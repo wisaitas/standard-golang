27a1a48a694c61f70ac78b7fc8082dc7a5e33017
wisaitas kampanat <jiwwisaitad01@hotmail.com>
2025-06-22 20:31:53 +0700

    delete package

--- pkg/query.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-type PaginationQuery struct {
-	Page     *int    `query:"page"`
-	PageSize *int    `query:"page_size"`
-	Sort     *string `query:"sort"`
-	Order    *string `query:"order"`
-}
